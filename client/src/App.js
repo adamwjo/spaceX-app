69536095fd18c413d7cfa3dd9e4ebe1ea3505cf6
adamwjo <adamwjo@gmail.com>
2019-06-10 12:44:25 -0400

    cleaned up boilerplate and set up bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.png';
+import { Z_BLOCK } from 'zlib';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <img src={logo} alt="logo" style={{ width: 300, display: 'block', margin: 'auto'}}></img>
     </div>
   );
 }
