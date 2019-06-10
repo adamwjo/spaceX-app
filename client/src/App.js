b7424eecdbfcc1ce4b3ca1c83d42c5dd960de466
adamwjo <adamwjo@gmail.com>
2019-06-10 13:00:52 -0400

    set up apollo-graphql

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
 import logo from './logo.png';
-import { Z_BLOCK } from 'zlib';
+
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+});
 
 function App() {
+ 
   return (
-    <div className="App">
-     <img src={logo} alt="logo" style={{ width: 300, display: 'block', margin: 'auto'}}></img>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="container">
+        < img src={logo} alt="logo" style={{ width: 300, display: 'block', margin: 'auto'}}></img>
+      </div>
+    </ApolloProvider>
   );
 }
 
