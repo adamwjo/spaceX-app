5017c457e1abd0a4ca99536e677b8048cc8d1542
adamwjo <adamwjo@gmail.com>
2019-06-14 14:19:20 -0400

    set up app for deployment

---
+++ public/precache-manifest.88dc4831dd7534c920ae9febc57697f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de0961cdd994e21cd9109b4feba723e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a9c121c6aee83b6a5ae",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "c1eed61e25c8c434cef5",
+    "url": "/static/js/2.f7138084.chunk.js"
+  },
+  {
+    "revision": "1a9c121c6aee83b6a5ae",
+    "url": "/static/js/main.d2d2f308.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema.js');
 const cors = require('cors');
+const path = require('path')
 
 
 //initialize app
@@ -11,8 +12,11 @@ const app = express();
 // Allow cross-origin
 app.use(cors());
 
+app.use(express.static('publiic'));
 
-
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+})
 
 app.use('/graphql', graphqlHTTP({
     schema,
